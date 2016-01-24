24bbaeb94871725cff5ed3be357b63f538e2681d
Chase Kernan <ckkernan@amazon.com>
2016-01-24 13:41:26 -0800

    Stage 0 babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import operators from './lang/operators'
+
 export const N = {
     version: '0.0.0',
+    ...operators
 }
 
 export default N
---
+++ src/lang/operators.js
@@ -0,0 +1,5 @@
+export default {
+    ['+'](left, right) {
+        return left + right
+    }
+}
